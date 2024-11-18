f4b044cdc6c0bdb0d6923cbd4bbd5e9cd1459a06
Arman <mdarman365506@gmail.com>
2024-11-19 00:36:02 +0600

    set return login page if user not logged for see brandDetails

--- src/private/BrandDetails.jsx
+++ src/private/BrandDetails.jsx
@@ -1,9 +1,22 @@
-import { useLoaderData } from "react-router-dom";
+import { useContext, useEffect } from "react";
+import { useLoaderData, useLocation, useNavigate } from "react-router-dom";
+import { AuthContext } from "../provider/AuthProvider";
 
 
 const BrandDetails = () => {
     const brand = useLoaderData();
     console.log(brand);
+    const naviget = useNavigate();
+    const { user } = useContext(AuthContext)
+    
+    
+
+    useEffect(() => {
+        if (!user) {
+          return naviget("/login");
+        }
+    },[])
+   
     return (
         <div>
             brand details.....
