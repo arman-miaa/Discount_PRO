a8e10a3aeb63732b2b500acedba19aac3bddbec4
Arman <mdarman365506@gmail.com>
2024-11-19 23:59:12 +0600

    added linked option on home page for marque logo

--- src/components/BrandsSlide.jsx
+++ src/components/BrandsSlide.jsx
@@ -1,26 +1,40 @@
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Marquee from "react-fast-marquee";
-
 
 const BrandsSlide = () => {
   const [images, setImages] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
-    fetch('/collection.json')
-      .then(res => res.json())
-    .then(data=>setImages(data))
-  },[])
-  // console.log(images);
+    fetch("/collection.json")
+      .then((res) => res.json())
+      .then((data) => setImages(data));
+  }, []);
+
+  const handleLogoClick = (brandId) => {
+
+    navigate(`/brandDetails/${brandId}`);
+  };
+
   return (
     <div>
-      brand slide
-      <div className="border-2 flex">
+      <h2 className="text-xl font-semibold">Brand Slide</h2>
+      <div className="border-2 flex items-center">
         <button className="btn">All Category</button>
         <Marquee pauseOnHover={true} speed={50}>
           <div className="flex justify-between gap-8">
             {images.map((image) => (
-              <div className=" bg-blue-300" key={image._id} image={image}>
-                <img className="w-20 h-12" src={image.brand_logo} alt="" />
+              <div
+                className="cursor-pointer bg-blue-300 p-2"
+                key={image._id}
+                onClick={() => handleLogoClick(image._id)} 
+              >
+                <img
+                  className="w-20 h-12"
+                  src={image.brand_logo}
+                  alt={image.brand_name || "Brand Logo"}
+                />
               </div>
             ))}
           </div>
